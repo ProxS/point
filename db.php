af9198896318999e4fc70759e480d78fb55ddfb3
ProxS <sergey_proskuryakov_93@mail.ru>
2016-09-16 16:15:31 +0300

    physic model

--- db.php
+++ db.php
@@ -1,120 +1,108 @@
-<?php
+п»ї<?php
 
-//получаем данные на основе предыдущих 0 SQL
-function GetNext($vx0, $vy0, $posx0, $posy0, $an, $at, $ang,$dt) {
+define('MIN_V', -10);
+define('MAX_V', 10);
+define('MIN_POS', -10);
+define('MAX_POS', 10);
+
+//РїРѕР»СѓС‡Р°РµРј РґР°РЅРЅС‹Рµ РЅР° РѕСЃРЅРѕРІРµ РїСЂРµРґС‹РґСѓС‰РёС… 0 SQL
+function GetNext($vx0, $vy0, $posx0, $posy0, $an, $at, $ang, $dt) {
     $a_total = sqrt($at * $at + $an * $an);
 
-//convert градусов в радианы
-    $rad = $ang * M_PI / 180;
-
+//convert РіСЂР°РґСѓСЃРѕРІ РІ СЂР°РґРёР°РЅС‹
+    $rad = $ang*$dt;
+    $ang_g = $ang*180/M_PI;
 //https://yandex.ru/images/search?text=%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%86%D0%B8%D1%8F%20%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%B0%20%D1%83%D1%81%D0%BA%D0%BE%D1%80%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BD%D0%B0%20%D0%BE%D1%81%D1%8C&noreask=1&img_url=http%3A%2F%2Fmognovse.ru%2Fmogno%2F975%2F974870%2F974870_html_2d039d81.jpg&pos=10&rpt=simage&lr=213    
     $ax = $a_total * cos($rad);
     $ay = $a_total * sin($rad);
 
     $vx = $vx0 + $ax * $dt;
     $vy = $vy0 + $ay * $dt;
-
+    
+    var_dump($at, $an);
+    var_dump(cos($rad));
+    var_dump($rad);
+    
     $posx = $posx0 + $vx0 * $t + ($ax * $dt * $dt) / 2;
     $posy = $posy0 + $vy0 * $t + ($ay * $dt * $dt) / 2;
 
 
-    return array('vx' => $vx, 'vy' => $vy, 'posx' => $posx, 'posy' => $posy, 'ang' => $ang);
-//    var_dump($vx, $vy, $posx, $posy, $ang);
+    return array('vx' => $vx, 'vy' => $vy, 'posx' => $posx, 'posy' => $posy, 'ang' => $ang_g);
 }
 
-//получаем данные на указанное время 2 SQL
+//РїРѕР»СѓС‡Р°РµРј РґР°РЅРЅС‹Рµ РЅР° СѓРєР°Р·Р°РЅРЅРѕРµ РІСЂРµРјСЏ 2 SQL
 function GetPrev($created) {
 
-//хранить дату минуты сек в  поле инт 8
-//если записи нет в теч мин то сгенерировать
+//С…СЂР°РЅРёС‚СЊ РґР°С‚Сѓ РјРёРЅСѓС‚С‹ СЃРµРє РІ  РїРѕР»Рµ РёРЅС‚ 8
+//РµСЃР»Рё Р·Р°РїРёСЃРё РЅРµС‚ РІ С‚РµС‡ РјРёРЅ С‚Рѕ СЃРіРµРЅРµСЂРёСЂРѕРІР°С‚СЊ
     $query = "Select * from points where created <= $created  and created >= $created-60*1000 ORDER BY created desc limit 1";
     $result = mysql_query($query) or die('Error' . mysql_error());
     $line = mysql_fetch_array($result);
-    
-    $query_a = "Select * from points_property where created <= $created ORDER BY created desc limit 1";
+
+    //TODO РїРѕС‡РµРјСѓ РѕРіСЂР°РЅРёС‡РµРЅРёРµ С‚РѕР»СЊРєРѕ СЃ 1 СЃС‚РѕСЂРѕРЅС‹ ?
+    $query_a = "Select * from points_property where created <= $created  and created >= $created-60*1000 ORDER BY created desc limit 1";
     $result_a = mysql_query($query_a) or die('Error' . mysql_error());
     $line_a = mysql_fetch_array($result_a);
-    
-    if (empty($line)) {
 
-	$vx = rand(-10, 10);
-	$vy = rand(-10, 10);
-	$posx = rand(-10, 10);
-	$posy = rand(-10, 10);
-	$created = microtime(true)*1000;
-	$ang = rand(0, 360);
+    if (empty($line)) {
+//TODO С„РѕСЂРјР°С‚РёСЂРѕРІР°РЅРёРµ
+//TODO РіРґРµ РѕР±СЂР°Р±РѕС‚РєР° РјР°РєСЃРёРјР°Р»СЊРЅС‹С… Рё РјРёРЅРёРјР°Р»СЊРЅС‹С… Р·РЅР°С‡РµРЅРёР№?
+	$vx = rand(MIN_V, MAX_V);
+	$vy = rand(MIN_V, MAX_V);
+	$posx = rand(MIN_POS, MAX_POS);
+	$posy = rand(MIN_POS, MAX_POS);
+	//TODO РїРѕС‡РµРјСѓ СѓРјРЅРѕР¶Р°РµРј РЅР° 1000? РїРµСЂРµРІРѕРґРёРј РІ РјРёР»Р»РёСЃРµРє
+	$created = microtime(true) * 1000;
 	$an = $line_a['an'];
 	$at = $line_a['at'];
-	$ang = $line_a['ang'];
-
+	//TODO РџРѕС‡РµРјСѓ? $line_a['ang'] СЌС‚Рѕ РЅРµ РЅР°РїСЂР°РІР»РµРЅРёРµ РґРІРёР¶РµРЅРёСЏ
+//	$ang = $line_a['ang'];
+	
+	$ang = 0;
+	
 	$query = "Insert into points(vx,vy,posx,posy,created,ang) values($vx,$vy,$posx,$posy,$created,$ang)";
+
 	$result = mysql_query($query) or die('Error' . mysql_error());
 	
-//	var_dump(array($vx, $vy, $posx, $posy, $created, $an, $at, $ang));
 	return array('vx' => $vx, 'vy' => $vy, 'posx' => $posx, 'posy' => $posy, 'created' => $created, 'an' => $an, 'at' => $at, 'ang' => $ang);
     } else {
-	$vx = $line['vx'];
-	$vy = $line['vy'];
-	$posx = $line['posx'];
-	$posy = $line['posy'];
-	$created = $line['created'];
-	$an = $line_a['an'];
-	$at = $line_a['at'];
-	$ang = $line_a['ang'];
-	
-//	var_dump(array($vx, $vy, $posx, $posy, $created, $an, $at, $ang));
-	return array('0' => $vx, 'vy' => $vy, 'posx' => $posx, 'posy' => $posy, 'created' => $created, 'an' => $an, 'at' => $at, 'ang' => $ang);
+	return array('vx' => $line['vx'], 'vy' => $line['vy'], 'posx' => $line['posx'], 'posy' => $line['posy'],
+	    'created' => $line['created'], 'an' => $line_a['an'], 'at' => $line_a['at'], 'ang' => $line_a['ang']);
     }
 }
 
-//по обращению получение данных на экран
-//заполнение проперти проверка ограничений vx vy posx posy
-//заполнение данных по ускорению отдельно во 2 файле
+//РїРѕ РѕР±СЂР°С‰РµРЅРёСЋ РїРѕР»СѓС‡РµРЅРёРµ РґР°РЅРЅС‹С… РЅР° СЌРєСЂР°РЅ
+//Р·Р°РїРѕР»РЅРµРЅРёРµ РїСЂРѕРїРµСЂС‚Рё РїСЂРѕРІРµСЂРєР° РѕРіСЂР°РЅРёС‡РµРЅРёР№ vx vy posx posy
+//Р·Р°РїРѕР»РЅРµРЅРёРµ РґР°РЅРЅС‹С… РїРѕ СѓСЃРєРѕСЂРµРЅРёСЋ РѕС‚РґРµР»СЊРЅРѕ РІРѕ 2 С„Р°Р№Р»Рµ
 
 function getPoint($created) {
-    include 'generation_property.php';
-    
     $connection = mysql_connect('localhost', 'root', '')
-	    or die('Ошибка соединения: ' . mysql_error());
+	    or die('РћС€РёР±РєР° СЃРѕРµРґРёРЅРµРЅРёСЏ: ' . mysql_error());
 
     mysql_select_db('point', $connection)
 	    or die('Could not select database.');
-    
-    generation($created);
-    $list = GetPrev($created);
-    
-    $dt = ($created - $list['created'])/1000;
-    
-    var_dump($dt);
-    
-    $next = GetNext($list['vx'], $list['vy'], $list['posx'], $list['posy'], $list['an'], $list['at'], $list['ang'], $dt);
 
+    $list = GetPrev($created);
+
+    $dt = ($created - $list['created']) / 1000;
+    
+    $ang = $list['ang']*$dt;
+    
+    $next = GetNext($list['vx'], $list['vy'], $list['posx'], $list['posy'], $list['an'], $list['at'], $ang, $dt);
+    var_dump($next);
     $vx = $next['vx'];
     $vy = $next['vy'];
     $posx = $next['posx'];
     $posy = $next['posy'];
     $ang = $next['ang'];
-    
-    /*
-    if($posx>100 || $posx< -100 || $posy>100 || $posy< -100){
-	if($posx>100 || $posx < -100){
-	    $vx *= -1;
-	}else {
-	    $vy *= -1;
-	}
-    }
-     * 
-     */
-    
+
     $query = "Insert into points(vx,vy,posx,posy,created,ang) values($vx,$vy,$posx,$posy,$created,$ang)";
     $result = mysql_query($query) or die('Error' . mysql_error());
-    
-    var_dump($next);
+
 
     mysql_close($connection);
-    
+
     return $next;
 }
 
-getPoint(microtime(true)*1000);
-
+getPoint(microtime(true) * 1000);
--- generation_property.php
+++ generation_property.php
@@ -1,13 +1,38 @@
 <?php
 
+define('MIN_A', 0);
+define('MAX_A', 10);
+define('MIN_W', -10);
+define('MAX_W', 10);
+
 function generation ($created) {
     
-    $an = rand(0, 10);
-    $at = rand(0, 10);
-    $ang = rand(0, 360);
+    $an = rand(MIN_A, MAX_A);
+    $at = rand(MIN_A, MAX_A);
+    $ang = rand(MIN_W, MAX_W);
     
     $query = "Insert into points_property values($an,$at,$created,$ang)";
     $result = mysql_query($query) or die('Error' . mysql_error());
     
 }
 
+function countGenegation ($created) {
+    
+    $connection = mysql_connect('localhost', 'root', '')
+	    or die('Ошибка соединения: ' . mysql_error());
+
+    mysql_select_db('point', $connection)
+	    or die('Could not select database.');
+    
+    $createdstep = $created;
+    $count = 50;
+    
+    for($i = 0; $i < $count; $i++) {
+	generation($createdstep);
+	$createdstep += 1000;
+    }
+    
+    mysql_close($connection);
+}
+
+countGenegation(microtime(true)*1000);
